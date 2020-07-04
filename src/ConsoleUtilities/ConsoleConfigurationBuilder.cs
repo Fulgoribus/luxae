c3cde390eec1afb0439e211c9a10cd35dcff3f97
Hecadeximal <67809547+Hecadeximal@users.noreply.github.com>
2020-07-04 01:02:25 -0400

    Replace Entity Framework Migrations with DbUp. (#2)

---
+++ src/ConsoleUtilities/ConsoleConfigurationBuilder.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace Fulgoribus.ConsoleUtilities
+{
+    public static class ConsoleConfigurationBuilder
+    {
+        /// <summary>
+        /// Create an <see cref="IConfigurationRoot"/> using the standard locations/order outside a hosted environment.
+        /// </summary>
+        /// <param name="args">Console application command-line arguments.</param>
+        /// <param name="appAssembly">Assembly of the console app configuration is being created for. (Required for user secrets.)</param>
+        /// <returns><see cref="IConfigurationRoot"/> from JSON configuration, user secrets, environment variables, and the command line.</returns>
+        /// <remarks>Supporting a null <paramref name="args"/> because </remarks>
+        public static IConfigurationRoot BuildConfiguration(string[]? args, Assembly? appAssembly = null)
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+
+            // Determine what environment we're running in.
+            configurationBuilder.AddEnvironmentVariables(prefix: "DOTNET_");
+            if (args != null)
+            {
+                configurationBuilder.AddCommandLine(args);
+            }
+            var environment = new ConsoleHostEnvironment(configurationBuilder.Build());
+
+            // Build application settings following the same order as HostBuilder/WebHostBuilder.
+            configurationBuilder.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+            if (environment.IsDevelopment() && appAssembly != null)
+            {
+                configurationBuilder.AddUserSecrets(appAssembly, optional: true);
+            }
+            configurationBuilder.AddEnvironmentVariables();
+            if (args != null)
+            {
+                configurationBuilder.AddCommandLine(args);
+            }
+
+            return configurationBuilder.Build();
+        }
+    }
+}
--- src/DbUp/Program.cs
+++ src/DbUp/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Reflection;
+using DbUp;
+using Fulgoribus.ConsoleUtilities;
+using Microsoft.Extensions.Configuration;
 
 namespace Fulgoribus.Luxae.DbUp
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var configuration = ConsoleConfigurationBuilder.BuildConfiguration(args, assembly);
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            var upgrader = DeployChanges.To.SqlDatabase(connectionString)
+                .WithScriptsEmbeddedInAssembly(assembly)
+                .WithTransactionPerScript()
+                .LogToConsole()
+                .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
         }
     }
 }
