94aeb4a965f7d946af2523e06e95927d16ed5406
Hecadeximal <67809547+Hecadeximal@users.noreply.github.com>
2020-07-03 23:14:02 -0400

    Create solution/projects from templates.

---
+++ tests/UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fulgoribus.Luxae.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
