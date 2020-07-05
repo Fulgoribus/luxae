4284de9d0b8f6a84110c877a1e03df14e357ec84
Hecadeximal <67809547+Hecadeximal@users.noreply.github.com>
2020-07-05 12:41:14 -0400

    Support external identity providers in Luxae (#6)
    * Enable QR codes for MFA.
    
    * Enable email confirmation.
    
    * Disable phone numbers.
    
    * Add Google authentication.
    
    * Add Microsoft Account logins.
    
    * Add Twitter login support.
    
    * Add Privacy Policy and Terms of Service.
    
    * Use libman to restore JavaScript at build time.
    
    * Use CDN for all JavaScript.
    
    * Do not require configuration outside the web project; make the DAL work with a generic IDbConnection.
    
    * Enable webapp deploy to Azure.

---
+++ src/Web/Services/Email/SendGridOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Fulgoribus.Luxae.Web
+{
+    public class SendGridOptions
+    {
+        internal const string SectionName = "SendGrid";
+
+        public string ApiKey { get; set; } = string.Empty;
+
+        public string FromEmail { get; set; } = string.Empty;
+
+        public string FromName { get; set; } = string.Empty;
+    }
+}
--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Data;
 using Lamar;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,10 +26,53 @@ namespace Fulgoribus.Luxae.Web
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true);
             services.AddRazorPages();
 
-            // Load our custom services *after* Microsoft's so they win.
+            // Build authentication configuration. Can't use fluent syntax because we want to support running without configurations in place.
+            var authBuilder = services.AddAuthentication();
+
+            var googleOptions = Configuration.GetSection("Authentication:Google");
+            if (googleOptions.Exists())
+            {
+                authBuilder.AddGoogle(configureOptions =>
+                {
+                    configureOptions.ClientId = googleOptions["ClientId"];
+                    configureOptions.ClientSecret = googleOptions["ClientSecret"];
+                });
+            }
+
+            var microsoftOptions = Configuration.GetSection("Authentication:Microsoft");
+            if (microsoftOptions.Exists())
+            {
+                authBuilder.AddMicrosoftAccount(configureOptions =>
+                {
+                    configureOptions.ClientId = microsoftOptions["ClientId"];
+                    configureOptions.ClientSecret = microsoftOptions["ClientSecret"];
+                });
+            }
+
+            var twitterOptions = Configuration.GetSection("Authentication:Twitter");
+            if (twitterOptions.Exists())
+            {
+                authBuilder.AddTwitter(configureOptions =>
+                {
+                    configureOptions.ConsumerKey = twitterOptions["ConsumerKey"];
+                    configureOptions.ConsumerSecret = twitterOptions["ConsumerSecret"];
+                    configureOptions.RetrieveUserDetails = true;
+                });
+            }
+
+            // Load our custom services *after* Microsoft's so that our services win.
+
+            // Bind options pattern classes.
+            services.Configure<SendGridOptions>(Configuration.GetSection(SendGridOptions.SectionName));
+            services.Configure<TwoFactorAuthenticationOptions>(Configuration.GetSection(TwoFactorAuthenticationOptions.SectionName));
+
+            // Need to use a lamba to resolve the SqlConnection because trying to bind by type was going off into setter injection land.
+            services.For<IDbConnection>().Use(_ => new SqlConnection(Configuration.GetConnectionString("DefaultConnection"))).Scoped();
+
+            // Load Lamar registries in our DLLs. (These are our preferred way of resolving things that are not part of the framework; but anything
+            // needing IConfiguration like above is more easily handled in this method.)
             services.Scan(s =>
             {
-                // Look for any registry in a DLL we built.
                 s.AssembliesFromApplicationBaseDirectory(f => f?.FullName?.StartsWith("Fulgoribus.", StringComparison.OrdinalIgnoreCase) ?? false);
 
                 s.LookForRegistries();
